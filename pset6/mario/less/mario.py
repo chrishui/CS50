16d3f1fc9b294c3e17c3f0f44f81306baa4b3516
chrishui <christopher.slhui@gmail.com>
2020-11-19 07:48:50 +0000

    Update mario.py

--- pset6/mario/less/mario.py
+++ pset6/mario/less/mario.py
@@ -2,27 +2,21 @@ from cs50 import get_int
 
 # Main function
 def main():
-    # Obtain half-pyramid's height from user
     height = get_positive_int()
     # Initialize constants:
     i = 0
     h = height
     # Make pyramid
-    # Printing Spaces
     while h > 0 and i < height:
         print(" "*h + "#"*(i+1), end="\n")
         h -= 1
         i += 1
-    # Printing #s
-    #while i < height:
-        #print("#"*(i+1), end="\n")
-        #i += 1
 
-# Check if input is positive
+# Check if input is positive, and within range 0 to 8
 def get_positive_int():
     while True:
         n = get_int("Height: ")
-        if n > 0 and n < 8:
+        if n > 0 and n <= 8:
             break
     return n
 
