71c9b85681a86906e8db4001eecfc9c5669ad52e
chrishui <christopher.slhui@gmail.com>
2020-12-02 09:20:56 +0000

    Update 5.sql

---
+++ pset7/movies/5.sql
@@ -0,0 +1,2 @@
+/*  SQL query to list the titles and release years of all Harry Potter movies, in chronological order */
+SELECT year, title FROM movies WHERE title LIKE "Harry Potter%" ORDER BY year asc;
