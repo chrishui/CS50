0eea3804a9bd1c3953b26e33d6ae2f2321c51d70
chrishui <christopher.slhui@gmail.com>
2020-12-11 16:52:57 +0000

    Update 13.sql

---
+++ pset7/movies/13.sql
@@ -0,0 +1,21 @@
+/* SQL query to list the names of all people who starred in a movie in which Kevin Bacon also starred */
+/* Kevin Bacon born in 1958 */
+/* Kevin Bacon not included in resulting list */
+
+SELECT people.name FROM people WHERE people.id IN(
+
+SELECT stars.person_id FROM stars WHERE stars.movie_id IN (
+
+SELECT stars.movie_id FROM stars WHERE stars.person_id IN
+
+(SELECT people.id FROM people WHERE name = "Kevin Bacon" AND birth="1958")
+
+)
+
+EXCEPT
+
+SELECT people.id FROM people WHERE name = "Kevin Bacon" AND birth="1958"
+
+)
+;
+
